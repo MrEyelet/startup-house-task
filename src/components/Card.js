dd2fae185b4d4e2bd7b29abf6831be273d58f108
Marcin <marcin.adam.mroczko@gmail.com>
2020-10-18 10:15:41 +0200

    get user username and user image from github Api

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,28 @@ import styled from "styled-components"
 import axios from "axios"
 
 function Card() {
+  const gitHubUserUrl = "https://api.github.com/users/mreyelet"
+  const [userData, setUserData] = useState({})
+
+  useEffect(() => {
+    getGiHubUser()
+  }, [])
+
+  const getGiHubUser = async () => {
+    try {
+      console.log("everything ok")
+      const response = await axios.get(gitHubUserUrl)
+      setUserData(response.data)
+    } catch {
+      console.log("there was an error")
+    }
+  }
+
   return (
     <>
-      <p>username</p>
-      <a href="#" target="blank">
-        <img src="url" />
+      <p>{userData.name}</p>
+      <a href={userData.html_url} target="blank">
+        <img src={userData.avatar_url} alt={userData.login} />
       </a>
     </>
   )
