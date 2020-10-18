b8c6b97b187a8e3b55f28dba35ef264425dc897c
Marcin <marcin.adam.mroczko@gmail.com>
2020-10-18 10:08:29 +0200

    create static Card component

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
+import axios from "axios"
+
+function Card() {
+  return (
+    <>
+      <p>username</p>
+      <a href="#" target="blank">
+        <img src="url" />
+      </a>
+    </>
+  )
+}
+
+export default Card
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 import * as Section from "components/PageSection"
+import Card from "components/Card"
 //import Card from "components/Card"
 
 export const Profile = () => (
   <Section.Wrapper>
     <Section.Content>
-      <Section.Header>card component</Section.Header>
+      <Section.Header>
+        <Card />
+      </Section.Header>
     </Section.Content>
   </Section.Wrapper>
 )
